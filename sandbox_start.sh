5ef7e69912627345c11688223888be80a2762cac
Diego Hernandes <diego@hernandev.com>
2017-03-01 00:08:03 -0300

    sandbox - used for building packages only
    @TODO move all non public stuff to a directory names private

--- sandbox_start.sh
+++ sandbox_start.sh
@@ -5,4 +5,6 @@ mkdir -p $(pwd)/.abuild
 docker run -it --rm \
     -v $(pwd):/home/sandbox/php-alpine \
     -v $(pwd)/.abuild:/home/sandbox/.abuild \
+    -v $(pwd)/repo:/home/sandbox/packages/php-alpine \
+    -v $(pwd)/cache:/var/cache/apk \
     codecasts/php-alpine-sandbox
