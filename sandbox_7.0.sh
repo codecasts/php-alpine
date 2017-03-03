12e394f2b4f15afdcbdcb626c9aae682596ce244
Diego Hernandes <diego@hernandev.com>
2017-03-03 07:46:17 -0300

    fix build scripts and include support for XDebug

--- sandbox_7.0.sh
+++ sandbox_7.0.sh
@@ -3,9 +3,8 @@
 mkdir -p $(pwd)/.abuild
 
 docker run -it --rm \
-    -v $(pwd):/home/sandbox/php-alpine \
+    -v $(pwd)/7.0:/home/sandbox/7.0 \
     -v $(pwd)/.abuild:/home/sandbox/.abuild \
     -v $(pwd)/repo/7.0:/home/sandbox/packages/7.0 \
-    -v $(pwd)/repo/7.1:/home/sandbox/packages/7.1 \
     -v $(pwd)/cache:/var/cache/apk \
-    codecasts/php-alpine-sandbox
+    codecasts/php-alpine:7.0
