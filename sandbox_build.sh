26f868ca3566811eb7bb3e760db1d0c98d582d57
Diego Hernandes <diego@hernandev.com>
2017-03-03 07:46:17 -0300

    fix build scripts and include support for XDebug

--- sandbox_build.sh
+++ sandbox_build.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-docker build -t codecasts/php-alpine-sandbox sandbox
+docker build -f sandbox/Dockerfile-base -t codecasts/php-alpine:base sandbox
+docker build -f sandbox/Dockerfile-7.0 -t codecasts/php-alpine:7.0 sandbox
+docker build -f sandbox/Dockerfile-7.1 -t codecasts/php-alpine:7.1 sandbox
