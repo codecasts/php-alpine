078249e561c00173eeb1488181c01136e1dd4a40
Diego Hernandes <diego@hernandev.com>
2017-03-03 05:18:14 -0300

    split builds into two separate repositories

--- sandbox_start.sh
+++ sandbox_start.sh
@@ -5,6 +5,7 @@ mkdir -p $(pwd)/.abuild
 docker run -it --rm \
     -v $(pwd):/home/sandbox/php-alpine \
     -v $(pwd)/.abuild:/home/sandbox/.abuild \
-    -v $(pwd)/repo:/home/sandbox/packages/php-alpine \
+    -v $(pwd)/repo/7.0:/home/sandbox/packages/7.0 \
+    -v $(pwd)/repo/7.1:/home/sandbox/packages/7.1 \
     -v $(pwd)/cache:/var/cache/apk \
     codecasts/php-alpine-sandbox
