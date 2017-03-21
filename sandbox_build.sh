b299255ee9d530b2602a5a76cf1ff0b160ee55b8
Diego Hernandes <diego@hernandev.com>
2017-03-21 03:45:09 -0300

    update 7.1.3 and 7.0.17

--- sandbox_build.sh
+++ sandbox_build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker build -f sandbox/Dockerfile-base -t codecasts/php-alpine:base sandbox
-docker build -f sandbox/Dockerfile-7.0 -t codecasts/php-alpine:7.0 sandbox
-docker build -f sandbox/Dockerfile-7.1 -t codecasts/php-alpine:7.1 sandbox
+docker build --no-cache -f sandbox/Dockerfile-base -t codecasts/php-alpine:base sandbox
+docker build --no-cache -f sandbox/Dockerfile-7.0 -t codecasts/php-alpine:7.0 sandbox
+docker build --no-cache -f sandbox/Dockerfile-7.1 -t codecasts/php-alpine:7.1 sandbox
