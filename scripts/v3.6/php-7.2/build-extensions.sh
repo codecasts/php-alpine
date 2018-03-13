e2120d20f0c860e65c183559af654507d50f4852
Diego Hernandes <diego@hernandev.com>
2018-03-13 16:22:01 -0300

    update the extension building script

---
+++ scripts/v3.6/php-7.2/build-extensions.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+
+# SUB-PACKAGES TO BUILD
+PACKAGES="argon2 php7-apcu php7-imagick php7-libsodium php7-memcached php7-mongodb php7-redis php7-ssh2 php7-xdebug"
+
+# ROOT PATH
+ROOT_PATH=$(pwd)
+
+# cd $ROOT_PATH/php7
+# abuild -r
+
+# update packages.
+sudo apk update
+
+for PACKAGE in $PACKAGES; do
+    echo "----> Building $PACKAGE"
+    cd $ROOT_PATH/$PACKAGE
+    abuild checksum
+    abuild -r
+done
+
+# RETURN TO ROOT PATH.
+cd $ROOT_PATH
