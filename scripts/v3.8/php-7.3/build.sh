80d57a2429579709922d91b6f36565780e345772
Diego Hernandes <iamhernandev@gmail.com>
2018-12-13 13:16:16 -0200

    php 7.3

--- scripts/v3.8/php-7.3/build.sh
+++ scripts/v3.8/php-7.3/build.sh
@@ -3,7 +3,7 @@
 # dependencies to build.
 PACKAGES="argon2 secp256k1"
 # extensions to build.
-EXTENSIONS="amqp apcu ast imagick libsodium memcached mongodb redis secp256k1 xdebug"
+EXTENSIONS="apcu ast amqp imagick libsodium mailparse memcached mongodb redis secp256k1 ssh2 xdebug"
 
 # define root packages source path.
 SOURCES_PATH=$(pwd)
