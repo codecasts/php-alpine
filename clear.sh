4244a45e5e6f7ee076624a21a93c5b6b2483f49e
Diego Hernandes <iamhernandev@gmail.com>
2019-02-03 20:21:06 -0200

    alpine 3.9 builds starter files; custom location for temp files to allow builds on non-linux hosts
    remove env sourcing from cleaning script
    
    remove wrongly commited files
    
    delete stuff

--- clear.sh
+++ clear.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-# source environment variables for the time being.
-source ./.env
-
-# find inside scripts folder, anything matching php7, remove sub folders src.
+# find inside scripts folder, anything matching php7, remove sub and pkg folders src.
 find ./scripts -type d | grep -e 'php7-[a-z]' | xargs -I "{a}" rm -rf  "{a}/src"
+find ./scripts -type d | grep -e 'php7-[a-z]' | xargs -I "{a}" rm -rf  "{a}/pkg"
