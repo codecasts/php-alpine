4e49d514369c6b1d6e097a56e7532bbe0e12528d
Diego Hernandes <iamhernandev@gmail.com>
2019-02-03 20:21:23 -0200

    fix clear script

--- clear.sh
+++ clear.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 # find inside scripts folder, anything matching php7, remove sub and pkg folders src.
-find ./scripts -type d | grep -e 'php7-[a-z]' | xargs -I "{a}" rm -rf  "{a}/src"
-find ./scripts -type d | grep -e 'php7-[a-z]' | xargs -I "{a}" rm -rf  "{a}/pkg"
+find ./scripts -type d -maxdepth 3 | grep -e 'php-7[0-9]*\.[0-9]*/' | xargs -I "{a}" rm -rf  "{a}/src"
+find ./scripts -type d -maxdepth 3 | grep -e 'php-7[0-9]*\.[0-9]*/' | xargs -I "{a}" rm -rf  "{a}/pkg"
