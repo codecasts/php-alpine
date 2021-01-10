5970d3bbe3749cbf7947c49208d8b0f1d3f396af
Diego Hernandes <iamhernandev@gmail.com>
2021-01-10 10:10:12 -0300

    fix entry

--- sandbox/opt/php-alpine/entrypoint.sh
+++ sandbox/opt/php-alpine/entrypoint.sh
@@ -1,19 +1,19 @@
-#!/bin/sh
-
-# source /etc/profile
-. /etc/profile
+#!/usr/bin/env ash
 
 # trust the locally mounted public key and fix permissions.
 sudo cp /home/sandbox/.abuild/php-alpine.rsa.pub /etc/apk/keys/php-alpine.rsa.pub
 sudo chmod u=rw,go=r /etc/apk/keys/php-alpine.rsa.pub
 
-# fix home dir owner permissions.
-sudo chown -R sandbox:sandbox /home/sandbox
+# fix all packages permissions.
+sudo chown -R sandbox:sandbox "$HOME"/packages || echo -e ""
+# fix all packages ownership.
+sudo chmod -R 644 "$HOME"/packages/*/*/*.apk || echo -e ""
+sudo chmod -R 644 "$HOME"/packages/*/*/*.tar.gz || echo -e ""
 
 # determine cpu count.
-# ABUILD_JOBS=$(grep -c '^processor\s*:\s[0-9]*$' < /proc/cpuinfo)
+ABUILD_JOBS=$(grep -c '^processor\s*:\s[0-9]*$' < /proc/cpuinfo)
 # configure JOBS variable as cpu count on abuild.conf file.
-# sudo sed -i "/export JOBS=.*/c\export JOBS=$ABUILD_JOBS" /etc/abuild.conf
+sudo sed -i "/export JOBS=.*/c\export JOBS=$ABUILD_JOBS" /etc/abuild.conf
 
 # source env files generated during docker build.
 if [ -f ~/.build_env ]; then
